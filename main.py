9c8227f632025219f3835177b7d8935863261c6a
ChathuraHasanga44 <s92067130@ousl.lk>
2023-10-01 19:26:38 +0530

    add button and another background to this app.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.graphics import Color, Rectangle
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class MyApp(App):
+    def build(self):
+        # Create the root widget as a BoxLayout with yellow background
+        root = BoxLayout(orientation='vertical', spacing=10, padding=10,
+                         size_hint=(None, None), size=(400, 400),
+                         pos_hint={'center_x': 0.5, 'center_y': 0.5})
 
+        # Set the background color for the entire BoxLayout
+        with root.canvas.before:
+            Color(1, 1, 0, 1)  # Yellow background (RGBA color values)
+            rect = Rectangle(size=root.size, pos=root.pos)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        # Create a Button with a blue background
+        button = Button(text='View Certificate', background_color=(0, 0, 1, 1))  # Blue background
 
+        # Add the Button to the BoxLayout (on top of the yellow background)
+        root.add_widget(button)
 
-# Press the green button in the gutter to run the script.
+        return root
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    MyApp().run()
